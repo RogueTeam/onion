c075a18383eee0cf4be7696e6cae212cf64d7382
Antonio <antoniojosedonishung@gmail.com>
2025-08-18 15:35:40 -0300

    log: improved logging

--- p2p/dhtutils/utils.go
+++ p2p/dhtutils/utils.go
@@ -3,6 +3,7 @@ package dhtutils
 import (
 	"context"
 	"fmt"
+	"log"
 	"time"
 
 	dht "github.com/libp2p/go-libp2p-kad-dht"
@@ -10,6 +11,8 @@ import (
 )
 
 func WaitForBootstrap(ctx context.Context, h host.Host, d *dht.IpfsDHT) (err error) {
+	log.Println("[*] Waiting for bootstraping")
+	defer log.Println("[+] Bootstrap completed")
 	err = d.Bootstrap(ctx)
 	if err != nil {
 		return fmt.Errorf("failed to bootstrap dht: %w", err)
@@ -27,6 +30,9 @@ func WaitForBootstrap(ctx context.Context, h host.Host, d *dht.IpfsDHT) (err err
 			}
 			return nil
 		case <-ticker.C:
+			log.Println("[*] Current peerstore state:", h.Peerstore().Peers().Len())
+
+			log.Println("[*] Current routing table state:", len(d.RoutingTable().ListPeers()))
 			if h.Peerstore().Peers().Len() > 0 && len(d.RoutingTable().ListPeers()) > 0 {
 				return nil
 			}
